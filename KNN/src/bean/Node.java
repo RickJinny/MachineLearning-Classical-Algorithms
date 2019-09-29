d129fd5d0ed5078ae0b2eac58412d17db4c5bebf
RickJinny <yweijin@163.com>
2019-09-30 00:00:42 +0800

    knn
    knn update

---
+++ KNN/src/bean/Node.java
@@ -0,0 +1,28 @@
+package bean;
+
+import java.util.List;
+
+public class Node {
+
+    /**
+     * 样本标签
+     */
+    private String tag;
+
+    /**
+     * 特征向量
+     */
+    private List<Double> eigenVectors;
+
+
+    private double disFromNode;
+
+
+
+
+
+
+
+
+
+}
